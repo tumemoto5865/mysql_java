e35ad9bd688f851b44d20e552c35ad4d7ae6ceaa
umemoto <umemototoshiharu@yahoo.co.jp>
2018-11-28 16:37:33 +0900

    新規作成

---
+++ src/main/java/com/example/demo/MysqlJava1Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlJava1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlJava1Application.class, args);
+	}
+}
